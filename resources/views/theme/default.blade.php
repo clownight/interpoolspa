74a118943d514feb6721e29b7515a7455e7a943f
clownight <watcharagiat.bo@gmail.com>
2024-05-19 14:53:43 +0700

    Update default.blade.php

--- resources/views/theme/default.blade.php
+++ resources/views/theme/default.blade.php
@@ -31,12 +31,19 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}" />
     <!-- Google Tag Manager -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-W89Q7CM">
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>
+        window.addEventListener('DOMContentLoaded', (event) => {
+        window.onload = function() {
+            setTimeout(function() {
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-W89Q7CM');
+            }, 2000);
+        };
+    });
+
     </script>
     <!-- End Google Tag Manager -->
     @yield('css')
