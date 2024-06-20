b69666d052ac0d3ae9c1b97f1f013e4f1c904bc0
clownight <69068330+alwdev@users.noreply.github.com>
2024-06-20 13:10:56 +0700

    Update default.blade.php

--- resources/views/theme/default.blade.php
+++ resources/views/theme/default.blade.php
@@ -19,17 +19,12 @@
     <!-- Core Style Css -->
     <link rel="stylesheet" href="{{ asset('css/style.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}" />
-    <!-- Google Tag Manager -->
-    <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
-    <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-9PKZ8FC0NS&amp;l=dataLayer&amp;cx=c"></script>
-    <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-W89Q7CM"></script>
-    <script>
-        // (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        // new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        // j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        // 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        // })(window,document,'script','dataLayer','GTM-W89Q7CM');
-    </script>
+
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KPFC7MX2');</script>
     <!-- End Google Tag Manager -->
     @yield('css')
 </head>
@@ -77,13 +72,9 @@
     <!-- custom scripts -->
     <script src="{{ asset('js/scripts.min.js') }}"></script>
     <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W89Q7CM"
-        height="0" width="0" style="display:none;visibility:hidden">
-        </iframe>
-    </noscript>
-    <script type="text/javascript" id="" src="https://gateway.autodigi.net/bundle.js?wid=5d7b2458c9a1b90012fe8442"></script>
-        <!-- End Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPFC7MX2"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @yield('script')
 </body>
 </html>
