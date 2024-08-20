4db2d7a3522cbcff8d16ece4f82428252e61b291
alwdev <69068330+alwdev@users.noreply.github.com>
2024-08-20 17:26:48 +0700

    Update default.blade.php

--- resources/views/theme/default.blade.php
+++ resources/views/theme/default.blade.php
@@ -20,20 +20,20 @@
     <link rel="stylesheet" href="{{ asset('css/style.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}" />
 
-    <!-- Google Tag Manager -->
+<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KPFC7MX2');</script>
+    })(window,document,'script','dataLayer','GTM-W89Q7CM');</script>
     <!-- End Google Tag Manager -->
-
     @yield('css')
 </head>
 <body>
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPFC7MX2"
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W89Q7CM"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- End Google Tag Manager (noscript) -->
     <!-- ==================== Start Loading ==================== -->
 
@@ -80,9 +80,6 @@
     <script src="{{ asset('js/scripts.min.js') }}"></script>
 
     
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPFC7MX2"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     @yield('script')
 
